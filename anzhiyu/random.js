a384e9bdd54675b8a4fe46e5b118ede3af94c06a
Martin <443724205@qq.com>
2025-04-27 20:56:48 +0800

    Site updated: 2025-04-27 20:56:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/26/hello-world/","2025/04/26/测试/"];function toRandomPost(){
+var posts=["2025/04/26/hello-world/","2025/04/27/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
